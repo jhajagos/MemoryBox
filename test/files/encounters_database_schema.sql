7069b8a805a989c127437c7731f36e60c44a3d2e
Janos G. Hajagos <risk.limits@gmail.com>
2017-02-21 23:12:11 -0500

    Continued to flesh out the underying test database

--- test/files/encounters_database_schema.sql
+++ test/files/encounters_database_schema.sql
@@ -1,9 +1,28 @@
 /* SQLITE Schema for test database */
 
-create table encounters (encounter_number int);
+create table encounters (encounter_number int,
+  pseudo_mrn varchar(10),
+  admit_date_time datetime,
+  discharge_date_time datetime,
+  admit_type varchar(64),
+  admit_source varchar(64));
 
-create table encounter_dx (encounter_number int);
 
-create table encounter_documenter (encounter_number int);
+create table encounter_dx (encounter_number int,
+  diagnosis_type varchar(64),
+  sequence_id int,
+  diagnosis_code varchar(12)
+);
 
-create table patient (pseudo_mrn varchar(10));
+
+create table encounter_documents (encounter_number int,
+  document_type varchar(64),
+  document_date datetime,
+  content text
+);
+
+
+create table patient (pseudo_mrn varchar(10),
+  gender varchar(10),
+  date_of_birth datetime
+);
