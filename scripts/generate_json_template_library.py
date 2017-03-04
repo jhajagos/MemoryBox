cd579e8927d5805e7e828bc32beb3567c62b07a7
Janos G. Hajagos <risk.limits@gmail.com>
2017-03-03 22:21:40 -0500

    Added scripts skeletons

---
+++ scripts/generate_json_template_library.py
@@ -0,0 +1,10 @@
+import memorybox as mb
+import argparse
+
+def main():
+    arg_parse_obj = argparse.ArgumentParser(description="Generate a JSON library by reading JSON")
+    arg_obj = argparse.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/load_and_run_memory_boxes.py
@@ -0,0 +1,11 @@
+import memorybox as mb
+import argparse
+
+
+def main():
+    arg_parse_obj = argparse.ArgumentParser(description="Create and load a MemoryBox for tracking items in an external source")
+    arg_obj = argparse.parse_args()
+
+
+if __name__ == "__main__":
+    main()
