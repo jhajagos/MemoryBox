9645dea9b15cf444146ef4b6b6558b3761cdf1f0
janos <risk.limits@gmail.com>
2018-06-13 14:42:48 -0400

    Fixed export to pull only last items across one or more states

--- scripts/export_tracked_items.py
+++ scripts/export_tracked_items.py
@@ -14,30 +14,32 @@ def get_db_connection(config_dict, reflect_db=True):
     return connection, meta_data
 
 
-def main(connection, meta_data, memory_box_name, item_class_name, data_item_class_name, state_name, state_name_data,
-         file_name, look_back_until_date="2000-01-01"):
+def main(connection, meta_data, memory_box_name, item_class_name, data_item_class_name, state_names, file_name,
+         look_back_until_date="2000-01-01"):
 
     schema_dict = {"schema": meta_data.schema, "look_back_until_date": look_back_until_date}
 
-    query_string = """select ti.id as track_item_id, ti.item_class_id, ti.transaction_id, ti.state_id, s1.name as state_name,
-    s2.name as data_state_name,
+    query_string = """select ti.id as track_item_id, ti.item_class_id, ti.transaction_id, tiu.state_id, s.name as state_name,
     di.id as data_item_id, di.data_item_class_id, dic.name as data_item_clas_name, data
-from %(schema)s.track_items ti 
+  from %(schema)s.track_items ti 
   join %(schema)s.item_classes ic on ic.id = ti.item_class_id
   join %(schema)s.memory_boxes mb on mb.id = ic.memory_box_id
-  join %(schema)s.states s1 ON s1.id = ti.state_id
-  join %(schema)s.track_item_updates itu on itu.track_item_id = ti.id
-  join %(schema)s.states s2 on itu.state_id = s2.id
-  join %(schema)s.data_items di on di.track_item_update_id = itu.id
-  join %(schema)s.data_item_classes dic on dic.id = di.data_item_class_id
-  where s1.name = :state_name and s2.name = :state_name_data and dic.name = :data_item_class_name and mb.name = :memory_box_name and ic.name = :item_class_name
-    and ti.created_at >= '%(look_back_until_date)s'
-  order by ti.transaction_id desc, dic.id""" % schema_dict
+  join %(schema)s.track_item_updates tiu on ti.id = tiu.track_item_id
+  join 
+    (select max(id) as maximum_track_item_update_id from %(schema)s.track_item_updates tu 
+      where tu.state_id in (select id from %(schema)s.states where name = any(:state_names))
+       group by tu.track_item_id) t
+        on t.maximum_track_item_update_id = tiu.id
+  join %(schema)s.data_items di on di.track_item_update_id = tiu.id
+  join %(schema)s.data_item_classes dic on dic.id  = di.data_item_class_id
+  join %(schema)s.data_item_types dit on di.data_item_type_id = dit.id
+  join %(schema)s.states s on tiu.state_id = s.id
+  where dic.name = :data_item_class_name and ti.updated_at >= '%(look_back_until_date)s'
+   order by ti.transaction_id desc, dic.id""" % schema_dict
 
     sql_parameters = {"memory_box_name": memory_box_name, "item_class_name": item_class_name,
-                      "data_item_class_name": data_item_class_name, "state_name": state_name,
-                      "state_name_data": state_name_data
-                      }
+                      "data_item_class_name": data_item_class_name,
+                      "state_names": state_names}
 
     cursor = connection.execute(sa.text(query_string), **sql_parameters)
     field_list = []
@@ -71,48 +73,42 @@ from %(schema)s.track_items ti
                 csv_writer.writerow(row)
 
 
-arg_parse_obj = argparse.ArgumentParser(
-    description="Exports out a tracked item into a CSV file")
+if __name__ == "__main__":
+    arg_parse_obj = argparse.ArgumentParser(
+        description="Exports out a tracked item into a CSV file")
 
-arg_parse_obj.add_argument("-c", "--config-json-filename", dest="config_json_filename",
-                           help="JSON configuration file: see './test/testing_config.json.example'",
-                           default="./config.json")
+    arg_parse_obj.add_argument("-c", "--config-json-filename", dest="config_json_filename",
+                               help="JSON configuration file: see './test/testing_config.json.example'",
+                               default="./config.json")
 
-arg_parse_obj.add_argument("-m", "--memory-box-name", dest="memory_box_name", default=None)
+    arg_parse_obj.add_argument("-m", "--memory-box-name", dest="memory_box_name", default=None)
 
+    arg_parse_obj.add_argument("-n", "--item-class-name", dest="item_class_name", default=None,
+                               help="")
 
-arg_parse_obj.add_argument("-n", "--item-class-name", dest="item_class_name", default=None,
-                           help="")
+    arg_parse_obj.add_argument("-d", "--data-item-name", dest="data_item_class_name", default=None,
+                               help="")
 
-arg_parse_obj.add_argument("-d", "--data-item-name", dest="data_item_class_name", default=None,
-                           help="")
+    arg_parse_obj.add_argument("-s", "--state-names", dest="state_names", default=None,
+                               help="This is the state of the item when the data was extracted")
 
-arg_parse_obj.add_argument("-s", "--state-name", dest="state_name", default=None,
-                           help="This is the current state of the item that you want to export")
+    arg_parse_obj.add_argument("-f", "--file-name", dest="file_name", default=None,
+                               help="")
 
-arg_parse_obj.add_argument("-t", "--state-name-data", dest="state_name_data", default=None,
-                           help="This is the state of the item when the data was extracted")
+    arg_parse_obj.add_argument("-l", "--look-back-until-date", dest="look_back_until_date", default="2000-01-01")
 
-arg_parse_obj.add_argument("-f", "--file-name", dest="file_name", default=None,
-                           help="")
+    arg_obj = arg_parse_obj.parse_args()
 
-arg_parse_obj.add_argument("-l", "--look-back-until-date", dest="look_back_until_date", default="2000-01-01")
+    config_json_filename = arg_obj.config_json_filename
+    if not os.path.exists(config_json_filename):
+        raise IOError, "Configuration file: '%s' does not exist" % config_json_filename
 
-arg_obj = arg_parse_obj.parse_args()
+    with open(config_json_filename, "r") as f:
+        config_dict = json.load(f)
 
-config_json_filename = arg_obj.config_json_filename
-if not os.path.exists(config_json_filename):
-    raise IOError, "Configuration file: '%s' does not exist" % config_json_filename
+    connection, meta_data = get_db_connection(config_dict)
 
-with open(config_json_filename, "r") as f:
-    config_dict = json.load(f)
+    state_names = arg_obj.state_names.split(",")
 
-connection, meta_data = get_db_connection(config_dict)
-
-if arg_obj.state_name_data is None:
-    state_name_data = arg_obj.state_name
-else:
-    state_name_data = arg_obj.state_name_data
-
-main(connection, meta_data, arg_obj.memory_box_name, arg_obj.item_class_name, arg_obj.data_item_class_name,
-     arg_obj.state_name, arg_obj.state_name_data, arg_obj.file_name, arg_obj.look_back_until_date)
+    main(connection, meta_data, arg_obj.memory_box_name, arg_obj.item_class_name, arg_obj.data_item_class_name,
+         state_names, arg_obj.file_name, arg_obj.look_back_until_date)
