91ce3560fb4d6cb0f3b4353555bcf942110ab49b
janos <risk.limits@gmail.com>
2018-06-24 10:10:00 -0400

    Change tracking implementation added script for changes of production database

--- migrations/migrate_in_production_changed_data_items.py
+++ migrations/migrate_in_production_changed_data_items.py
@@ -1,1 +1,32 @@
 ### For production database to add migrations
+
+import json
+import sqlalchemy as sa
+
+
+def main(config):
+
+    schema = config["schema"]
+    connection_uri = config["connection_uri"]
+
+    engine = sa.create_engine(connection_uri)
+
+    connection = engine.connect()
+    meta_data = sa.MetaData(connection, schema=schema)
+    meta_data.reflect()
+
+    changed_data_items = sa.Table("changed_data_items", meta_data,
+                               sa.Column("id", sa.Integer, primary_key=True),
+                               sa.Column("initial_data_item_id", sa.ForeignKey("data_items.id")),
+                               sa.Column("changed_data_item_id", sa.ForeignKey("data_items.id")),
+                               sa.Column("created_at", sa.DateTime))
+
+    meta_data.create_all()
+
+    connection.execute('alter table "%s".data_items add column data_for_diff JSONB' % schema)
+
+
+if __name__ == "__main__":
+
+    with open("config.json") as f:
+        config = json.load(f)
